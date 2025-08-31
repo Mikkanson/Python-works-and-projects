95e206ebac1012f660a82def86ed05b6220a0e38
Mikkan <virakoudom.ly@gmail.com>
2025-08-31 09:13:40 +0700

    Add homepage layout with navigation

---
+++ Name.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+print("My name is", name)
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are an adult.")
+print(age)
---
+++ SHOP.py
@@ -0,0 +1,7 @@
+colombian = int(input("How much peso you have left? "))
+peruvian = int(input("How much soles you have left? "))
+brazilian = int(input("How much reais you have left? "))
+
+total = (colombia / 4050) + (peruvian / 3.5) + (brazilian / 5.4)
+
+print(total)
